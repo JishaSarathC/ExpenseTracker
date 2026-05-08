54dbe80becb274e66144baec3ac51779bac4cd53
JishaSarathC <jishasarath3@gmail.com>
2026-05-08 23:45:43 +0530

    expencetracker

--- src/components/Expences/Expens.jsx
+++ src/components/Expences/Expens.jsx
@@ -2,7 +2,7 @@ import "./Expens.css";
 export default function Expens({totalExpenses,openExpenseModal}){
     return(
         <div className="Econtainer">
-      <div className="exp">Expenses:<span className="expense">₹{Number(totalExpenses).toFixed(2)}</span></div>
+      <div className="exp">Expenses:<span className="expense">₹{Number(totalExpenses)}</span></div>
       <button className="butn" onClick={openExpenseModal}>+ Add Expense</button>
       </div>
     )
